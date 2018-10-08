ba0d77513955ebbd5a06c7f4aa2d90f169d12589
Olivier Decock <olivdeco1@arteveldehs.be>
2018-10-08 08:57:42 +0200

    Tinder

---
+++ js/script.js
@@ -0,0 +1,80 @@
+function data(){
+	fetch('https://randomuser.me/api?results=10')
+  	.then(function(response) {
+    return response.json();
+	})
+  .then(function(myJson) {
+    // console.log(JSON.stringify(myJson));
+    localStorage.setItem('testObject', JSON.stringify(myJson));
+	});
+};
+
+//Nieuw profiel tonen
+let nul=0;
+let active = '';
+function nextProfile(){
+    let newPerson = JSON.parse(localStorage.getItem('testObject'));
+    active = newPerson.results[nul];
+    showProfile(active);
+    console.log(active.name);
+    console.log(nul);
+    if(nul>=9){
+        nul=0;
+        data();
+    }else{
+        nul++;
+    }
+};
+
+function showProfile(gegevens){
+let img = document.createElement('img');
+img.src = gegevens.picture.large;
+document.getElementById('div').innerHTML = '<img src="' + gegevens.picture.large + '" >';
+
+let showFName = gegevens.name.first;
+let showLName = gegevens.name.last;
+let fullName = showFName + ' ' + showLName;
+document.getElementById('name').innerHTML = showFName + ' ' + showLName;
+
+let showAge = gegevens.dob.age;
+document.getElementById('age').innerHTML = showAge;
+
+let showLocation = gegevens.location.city;
+document.getElementById('place').innerHTML = showLocation;
+};
+nextProfile();
+
+
+let likes = new Array();
+let dislikes = new Array();
+
+//LIKE
+function like(){
+	likes.push(active.name.first + ' ' + active.name.last);
+	console.log('You like: ' + likes);
+	localStorage.setItem('LIKES', JSON.stringify(likes));
+	nextProfile();		
+}
+//DISLIKE
+function dislike(){
+	dislikes.push(active.name.first + ' ' + active.name.last);
+	console.log('You dislike: ' + dislikes);
+	localStorage.setItem('DISLIKE', JSON.stringify(dislikes));
+	nextProfile();	
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
