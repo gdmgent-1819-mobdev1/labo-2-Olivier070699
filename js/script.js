f313edbaf06f6354e39b6171f533f9a7b7c7735c
Olivier Decock <olivdeco1@arteveldehs.be>
2018-11-14 19:25:21 +0100

    2

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 function data(){
-	fetch('https://randomuser.me/api?results=10')
-  	.then(function(response) {
+    fetch('https://randomuser.me/api?results=10')
+    .then(function(response) {
     return response.json();
-	})
+    })
   .then(function(myJson) {
-    // console.log(JSON.stringify(myJson));
     localStorage.setItem('testObject', JSON.stringify(myJson));
-    	
-	});
+        
+    });
 };
 
 //Laden
 data();
 
-//Nieuw profiel tonen
+//Nieuw profiel laden
 let nul=0;
 let active = '';
 function nextProfile(){
-
     let newPerson = JSON.parse(localStorage.getItem('testObject'));
     active = newPerson.results[nul];
     showProfile(active);
@@ -31,6 +29,7 @@ function nextProfile(){
     }
 };
 
+// NIEUW PROFIEL TONEN
 function showProfile(gegevens){
 let img = document.createElement('img');
 img.src = gegevens.picture.large;
@@ -47,84 +46,73 @@ document.getElementById('age').innerHTML = 'Age: ' + showAge;
 let showLocation = gegevens.location.city;
 document.getElementById('place').innerHTML = showLocation;
 };
-nextProfile();
 
+nextProfile();
 
 let likes = new Array();
 let dislikes = new Array();
 
-//LIKE
+
+// LIKE
 function like(){
-	likes.push(active.name.first + ' ' + active.name.last);
-	console.log('You like: ' + likes);
-	localStorage.setItem('LIKES', JSON.stringify(likes));
-	nextProfile();	
-}
+    likes.push(active.name.first + ' ' + active.name.last);
+    console.log('You like: ' + likes);
+    localStorage.setItem('LIKES', JSON.stringify(likes));
+    nextProfile();
+    seeLikes();
+};
 //DISLIKE
 function dislike(){
-	dislikes.push(active.name.first + ' ' + active.name.last);
-	console.log('You dislike: ' + dislikes);
-	localStorage.setItem('DISLIKE', JSON.stringify(dislikes));
-	nextProfile();	
+    dislikes.push(active.name.first + ' ' + active.name.last);
+    console.log('You dislike: ' + dislikes);
+    localStorage.setItem('DISLIKE', JSON.stringify(dislikes));
+    nextProfile();
+    seeDislikes();
 };
 
-//SEE LIKES
+// seeLikes
 function seeLikes(){
-	document.getElementById('likeList').innerHTML = '';
-	for (let i = 0; i < likes.length; i++) {
-		document.getElementById('likeList').innerHTML += '<li class="like">' + likes[i] + '</li>';
-	};
+    document.getElementById('likeList').innerHTML = "";
+    for (let i = 0; i < likes.length; i++) {
+        document.getElementById('likeList').innerHTML += "<li class='li_likes' id='" + i + "'>" + likes[i] + "</li>";
+    }
+    renderEvtListeners();
 };
 
-//SEE DISLIKES
+// seeDislikes
 function seeDislikes(){
-	document.getElementById('dislikelist').innerHTML = '';
-	for (let i = 0; i < dislikes.length; i++) {
-		document.getElementById('dislikelist').innerHTML += '<li class="dislike">' + dislikes[i] + '</li>';
-	};
+    document.getElementById('dislikeList').innerHTML = "";
+    for (let i = 0; i < dislikes.length; i++) {
+        document.getElementById('dislikeList').innerHTML += "<li class='li_dislikes' id='" + i + "'>" + dislikes[i] + "</li>";
+    }
+    renderEvtListeners();
 };
 
-// CLEAR LISTS
-function clearAll(){
-	document.getElementById('likeList').innerHTML = '';
-	document.getElementById('dislikelist').innerHTML = '';	
+//Like to dislike
+function renderEvtListeners(){
+    let li_likes = document.getElementsByClassName("li_likes");
+    for (let i = 0; i < li_likes.length; i++) {
+        li_likes[i].addEventListener("click", toDislike);
+    }
+
+    let li_dislikes = document.getElementsByClassName("li_dislikes");
+    for (let i = 0; i < li_dislikes.length; i++) {
+        li_dislikes[i].addEventListener("click", toLike);
+    }
 }
 
-// LIKE NAAR DISLIKE
-for (let i = 0; i < likes.length; i++) {
-	let liLikes = document.getElementsByClassName('like');
-		liLikes[i].addEventListener("click", function(){
-			localStorage.setItem('DISLIKE', JSON.stringify(dislikes));
-			dislikes.push[i];
-			likes.splice(i, 1);
-			seeLikes();
-			seeDislikes();
-	})
-};
+function toDislike(e){
+    console.log('LIKEDEBUG: ' + e.currentTarget.id);
+    dislikes.push(likes[e.currentTarget.id]);
+    likes.splice(e.currentTarget.id, 1);
+    seeDislikes();
+    seeLikes();
+}
 
-// DISLIKE NAAR LIKE
-for (let i = 0; i < dislikes.length; i++) {
-		let liDislikes = document.getElementsByClassName('dislike');
-		liDislikes[i].addEventListener("click", function(){
-			localStorage.setItem('likes', JSON.stringify(likes));
-			likes.push[i];
-			dislikes.splice(i, 1);
-			seeLikes();
-			seeDislikes();
-	})
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function toLike(e){
+    console.log('DISLIKEDEBUG: ' + e.currentTarget.id);
+    likes.push(dislikes[e.currentTarget.id]);
+    dislikes.splice(e.currentTarget.id, 1);
+    seeDislikes();
+    seeLikes();
+}
